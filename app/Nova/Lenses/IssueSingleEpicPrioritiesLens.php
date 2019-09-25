60f15df25a9f14a1dee1eeceafe963fb87d33706
Tyler Reed <tyler.reed@tsico.com>
2019-09-25 15:20:02 -0500

    ~ Finalized single epic priorities lens

--- app/Nova/Lenses/IssueSingleEpicPrioritiesLens.php
+++ app/Nova/Lenses/IssueSingleEpicPrioritiesLens.php
@@ -3,8 +3,11 @@
 namespace App\Nova\Lenses;
 
 use Field;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Laravel\Nova\Http\Requests\LensRequest;
+use App\Models\WorkflowStatusGroup as WorkflowStatusGroupModel;
 
 class IssueSingleEpicPrioritiesLens extends Lens
 {
@@ -38,14 +41,32 @@ class IssueSingleEpicPrioritiesLens extends Lens
         // Apply the scope
         $scope($query);
 
+        // Select the relevant columns
+        $query->select([
+            'issues.type_icon_url',
+            'issues.key',
+            'issues.summary',
+            'issues.status_name',
+            'issues.status_color',
+            'workflow_status_groups.display_name as status_group_name',
+            'workflow_status_groups.color as status_group_color',
+            'issues.assignee_icon_url',
+            'issues.reporter_icon_url',
+            'issues.due_date',
+            'issues.estimate_date',
+            'issues.labels',
+            'issues.resolution_date',
+            'issues.entry_date'
+        ]);
+
         // Order by estimate, then by rank
         $query->orderBy('estimate_date', 'asc');
         $query->orderBy('rank', 'asc');
 
         // Return the query
-        return $request->withFilters(
+        return $request->withOrdering($request->withFilters(
             $query
-        );
+        ));
     }
 
     /**
@@ -62,16 +83,16 @@ class IssueSingleEpicPrioritiesLens extends Lens
                 return $this->type_icon_url;
             })->maxWidth(16),
 
-            Field::text('Key', 'key'),
+            Field::text('Key', 'key')->sortable(),
 
             Field::text('Summary', 'summary', function() {
                 return strlen($this->summary) > 100 ? substr($this->summary, 0, 100) . '...' : $this->summary;
             }),
 
-            Field::badgeUrl('Status', 'status_name')->backgroundUsing(function($value, $resource) {
-                return config("jira.colors.{$resource->status_color}.background");
+            Field::badgeUrl('Status', 'status_group_name')->backgroundUsing(function($value, $resource) {
+                return $resource->status_group_color;
             })->foregroundUsing(function($value, $resource) {
-                return config("jira.colors.{$resource->status_color}.color");
+                return '#000';
             })->style([
                 'fontFamily' => '\'Segoe UI\'',
                 'fontSize' => '12px',
@@ -81,6 +102,16 @@ class IssueSingleEpicPrioritiesLens extends Lens
                 'marginTop' => '0.25rem'
             ]),
 
+            Field::text('Week', 'labels')->displayUsing(function($value) {
+
+                $week = Arr::first($value, function($label) {
+                    return Str::startsWith($label, 'Week');
+                });
+
+                return empty($week) ? null : substr($week, 4);
+
+            }),
+
             Field::avatar('A')->thumbnail(function() {
                 return $this->assignee_icon_url;
             })->maxWidth(16),
@@ -89,9 +120,9 @@ class IssueSingleEpicPrioritiesLens extends Lens
                 return $this->reporter_icon_url;
             })->maxWidth(16),
 
-            Field::date('Due', 'due_date')->format('M/D'),
+            Field::date('Due', 'due_date')->format('M/D')->sortable(),
 
-            Field::date('Estimate', 'estimate_date')->format('M/D'),
+            Field::date('Estimate', 'estimate_date')->format('M/D')->sortable(),
 
         ];
 
@@ -107,8 +138,22 @@ class IssueSingleEpicPrioritiesLens extends Lens
     public function filters(Request $request)
     {
         return [
-            new \App\Nova\Filters\StatusIssueFilter
+            $this->getStatusFilter()
         ];
+    }
+
+    /**
+     * Creates and returns a new status filter.
+     *
+     * @return \Laravel\Nova\Filters\Filter
+     */
+    public function getStatusFilter()
+    {
+        // Determine the options
+        $options = WorkflowStatusGroupModel::orderBy('transition_order')->pluck('display_name', 'display_name')->all();
+
+        // Return the filter
+        return new \App\Nova\Filters\FluentSelectFilter('Status', 'status_group_name', $options);
     }
 
     /**
@@ -121,10 +166,42 @@ class IssueSingleEpicPrioritiesLens extends Lens
     public function cards(Request $request)
     {
         // Determine the scope
-        $scope = static::scope($this->epic);
+        $scope = static::scope($this->epic, true);
 
         // Return the cards
         return [
+            (new \App\Nova\Metrics\FluentValue)
+                ->model(\App\Models\Issue::class)
+                ->label('Remaining')
+                ->countOf('issues.id')
+                ->dateColumn(false)
+                ->suffix('issue')
+                ->useCurrentToRange()
+                ->scope($scope)
+                ->scopeWithRange(function($query, $range) {
+                    $query->where(function($query) use ($range) {
+                        $query->whereNull('issues.resolution_date');
+                        $query->orWhere('issues.resolution_date', '>=', $range[0]->copy()->addDays(30));
+                    });
+                }),
+
+            (new \App\Nova\Metrics\FluentTrend)
+                ->model(\App\Models\Issue::class)
+                ->label('Recent Completions')
+                ->countOf('issues.id')
+                ->dateColumn('resolution_date')
+                ->suffix('issue')
+                ->filter($scope),
+
+            (new \App\Nova\Metrics\FluentTrend)
+                ->model(\App\Models\Issue::class)
+                ->label('Upcoming Completions')
+                ->countOf('issues.id')
+                ->dateColumn('estimate_date')
+                ->suffix('issue')
+                ->futuristic()
+                ->filter($scope),
+
             (new \App\Nova\Metrics\IssueWeekStatusPartition)->setName('Last Week')->reference('-1 week')->filter($scope),
             (new \App\Nova\Metrics\IssueWeekStatusPartition)->setName('This Week')->filter($scope),
             (new \App\Nova\Metrics\IssueWeekStatusPartition)->setName('Next Week')->reference('+1 week')->filter($scope),
@@ -160,20 +237,27 @@ class IssueSingleEpicPrioritiesLens extends Lens
     /**
      * Returns the scope of this lens.
      *
+     * @param  string   $epic
+     * @param  boolean  $withComplete
+     *
      * @return \Closure
      */
-    public static function scope($epic)
+    public static function scope($epic, $withComplete = false)
     {
-        return function($query) use ($epic) {
+        return function($query) use ($epic, $withComplete) {
 
             // Only look at incomplete issues
-            $query->incomplete();
-
-            // Require an assignee
-            $query->assigned();
+            if(!$withComplete) {
+                $query->incomplete();
+            }
 
             // Filter to epic
             $query->where('epic_key', '=', $epic);
+
+            // Join into status groups
+            $query->joinRelation('status.group', function($join) {
+                $join->whereNull('workflow_status_types.scope_id');
+            });
 
         };
     }
