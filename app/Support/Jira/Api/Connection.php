4ad5904e86bc34cea44606e0b3857ba6c5904092
Tyler Reed <tyler.reed@tsico.com>
2020-03-03 18:28:04 -0600

    + Added additional endpoints

--- app/Support/Jira/Api/Connection.php
+++ app/Support/Jira/Api/Connection.php
@@ -325,6 +325,48 @@ class Connection extends ApiConnection
     }
 
     /**
+     * Returns the versions for the specified project.
+     *
+     * @param  string  $projectIdOrKey
+     *
+     * @return array
+     */
+    public function getProjectVersions($projectIdOrKey)
+    {
+        return $this->request()->path("project/{$projectIdOrKey}/versions")->get();
+    }
+
+    /**
+     * Returns the paginated versions for the specified project.
+     *
+     * @param  string  $projectIdOrKey
+     * @param  array   $options
+     *
+     * @option  {integer}       "startAt"     The page offset (defaults to 0).
+     * @option  {integer}       "maxResults"  The maximum number of items to return per page (defaults to 50, maximum is 50).
+     * @option  {string}        "orderBy"     The field to order the results by (default is "name").
+     * @option  {string}        "query"       The literal string used to filter by the versions by name or description (case insensitive).
+     * @option  {string|array}  "status"      The status value(s) to filter the versions.
+     * @option  {string|array}  "expand"      The additional information to include in the response.
+     *
+     * @return \stdClass
+     */
+    public function getProjectVersionsPaginated($projectIdOrKey, $options = [])
+    {
+        // Convert the "status" option to a string
+        if(isset($options['status']) && is_array($options['status'])) {
+            $options['status'] = implode(',', $options['status']);
+        }
+
+        // Convert the "expand" option to a string
+        if(isset($options['expand']) && is_array($options['expand'])) {
+            $options['expand'] = implode(',', $options['expand']);
+        }
+
+        return $this->request()->path("project/{$projectIdOrKey}/version", $options)->get();
+    }
+
+    /**
      * Returns the list of all issue resolution values.
      *
      * @return array
@@ -344,6 +386,18 @@ class Connection extends ApiConnection
     public function getStatuses()
     {
         return $this->request()->path('status')->get();
+    }
+
+    /**
+     * Returns a list of all of the status categories.
+     *
+     * @link https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-statuscategory-get
+     *
+     * @return array
+     */
+    public function getStatusCategories()
+    {
+        return $this->request()->path('statuscategory')->get();
     }
 
     /**
